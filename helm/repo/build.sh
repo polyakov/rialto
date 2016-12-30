a1ad70880be01418fdad15e505440a591cbfafc9
Vadim Polyakov <vadim@preparedmind.net>
2016-12-30 22:47:29 +0000

    add chart for openid

--- helm/repo/build.sh
+++ helm/repo/build.sh
@@ -5,4 +5,7 @@ cd charts
 rm ldap.ApacheDS-0.1.0.tgz
 tar -czvf ldap.ApacheDS-0.1.0.tgz ldap.ApacheDS/
 
+rm openid-connect-ldap-mitre-0.1.0.tgz
+tar -czvf openid-connect-ldap-mitre-0.1.0.tgz openid-connect-ldap-mitre/
+
 cd ..
